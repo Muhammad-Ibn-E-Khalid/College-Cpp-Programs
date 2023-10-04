ba3dac0da405eaa5ec97cf694012a1b8615d0c35
Muhammad-Ibn-E-Khalid <83428194+Muhammad-Ibn-E-Khalid@users.noreply.github.com>
2023-10-04 20:39:53 +0500

    Update BasicOperation.cpp

--- BasicOperation.cpp
+++ BasicOperation.cpp
@@ -6,50 +6,56 @@ int main()
 
     int option;
     float op1,op2,op;
-
-    cout<<"1: Sum \t\t 2: Subtract \n";
+    int n;
+	cin>>n;    
+    cout<<"Enter first number: ";
+    cin>>op1;
+    cout<<"Enter second number: ";
+    cin>>op2;
+    cout<<"Muhammad Bin Khalid";
+    cout<<"1: Sum \t\t 2: Subtraction \n";
     cout<<"3: Multipliply \t 4: Divide \n";
+    cout<<"5: Reminder 6:Exit\n";
     cout<<"Choosse Operation: ";
     cin>>option;
-    
-        if(option == 1)
+      
+	if(n==1){
+
+    if(option == 1)
 	{
-    	cout<<"Enter first number: ";
-    	cin>>op1;
-    	cout<<"Enter second number: ";
-    	cin>>op2;
     	op = op1+op2;
     	cout<<"Sum of two number = "<<op;
 	}
 	
 	else if(option == 2)
 	{
-    	cout<<"Enter first number: ";
-    	cin>>op1;
-    	cout<<"Enter second number: ";
-    	cin>>op2;
     	op = op1-op2;
     	cout<<"Subtract of two number = "<<op;
 	}
 	
 	else if(option == 3)
 	{
-     	cout<<"Enter first number: ";
-    	cin>>op1;
-    	cout<<"Enter second number: ";
-    	cin>>op2;
     	op = op1*op2;
     	cout<<"Multiplcation of two number = "<<op;
 	}
 	
 	else if(option == 4)
 	{
-		cout<<"Enter first number: ";
-    	cin>>op1;
-    	cout<<"Enter second number: ";
-    	cin>>op2;
     	op = op1/op2;
     	cout<<"Divide of two number = "<<op;
+	}
+	
+	else if(option == 5)
+	{
+    	cin>>op2;
+    //	op = op1 % op2;
+    	cout<<"Divide of two number = "<<op;
+	}
+	
+	else if(option == 6)
+	{
+		cout<<"";
+		return 0;
 	}
 	
 	else
@@ -60,6 +66,50 @@ int main()
 		main();
 	}
 	
+	
+}
+	
+	
+	
+	
+	
+	
+	else if(n==2)
+	{
+	
+	switch(option)
+    {
+    case 1:
+    	
+        op = op1+op2;
+        cout<<"Sum of two number = "<<op;
+    break;
+    
+	case 2:
+    
+	    op = op1-op2;
+        cout<<"Sum of two number = "<<op;
+    break;
+    
+    case 3:
+
+        op = op1*op2;
+        cout<<"Sum of two number = "<<op;
+    break;
+
+    case 4:
+
+        op = op1/op2;
+        cout<<"Sum of two number = "<<op;
+    break;
+
+    Defualt: 
+    cout<<"Invalid Option \n";
+    }
+}
+	
 	system("pause");
-        return 0;
+    system("cls");
+    main();
+
 }
